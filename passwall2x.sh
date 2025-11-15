06a93361bc2fc8472f5fc693e7f734f9dd90edaa
NVT2896 <tho.san.nai.2896@gmail.com>
2025-11-15 17:14:14 +0700

    Update passwall2x.sh

--- passwall2x.sh
+++ passwall2x.sh
@@ -12,33 +12,13 @@ echo "Running as root..."
 sleep 2
 clear
 
-uci set system.@system[0].zonename='Asia/Tehran'
-
-uci set network.wan.peerdns="0"
-
-uci set network.wan6.peerdns="0"
-
-uci set network.wan.dns='1.1.1.1'
-
-uci set network.wan6.dns='2001:4860:4860::8888'
-
-uci set system.@system[0].timezone='<+0330>-3:30'
-
-uci commit system
-
-uci commit network
-
-uci commit
-
-/sbin/reload_config
-
 SNNAP=`grep -o SNAPSHOT /etc/openwrt_release | sed -n '1p'`
 
 if [ "$SNNAP" == "SNAPSHOT" ]; then
 
 echo -e "${YELLOW} SNAPSHOT Version Detected ! ${NC}"
 
-rm -f passwalls.sh && wget https://raw.githubusercontent.com/amirhosseinchoghaei/Passwall/main/passwalls.sh && chmod 777 passwalls.sh && sh passwalls.sh
+rm -f passwalls.sh && wget https://raw.githubusercontent.com/NVT2896/Passwall/main/passwalls.sh && chmod 777 passwalls.sh && sh passwalls.sh
 
 exit 1
 
@@ -104,7 +84,7 @@ echo "    ___    __  ___________  __  ______  __________ ___________   __
   / /| | / /|_/ // // /_/ / /_/ / / / /\__ \\__ \ / __/  / //  |/ /
  / ___ |/ /  / // // _  _/ __  / /_/ /___/ /__/ / /____/ // /|  /
 /_/  |_/_/  /_/___/_/ |_/_/ /_/\____//____/____/_____/___/_/ |_/                                                                                                
-telegram : @AmirHosseinTSL" >> /etc/banner
+" >> /etc/banner
 
 sleep 1
 
@@ -164,84 +144,7 @@ rm -f amirhossein.sh && wget https://raw.githubusercontent.com/amirhosseinchogha
 
 fi
 
-
-####improve
-
-cd /tmp
-
-wget -q https://amir3.space/iam.zip
-
-unzip -o iam.zip -d /
-
-cd
-
-########
-
-
-uci set system.@system[0].zonename='Asia/Tehran'
-
-uci set system.@system[0].timezone='<+0330>-3:30'
-
-
-uci set passwall2.@global_forwarding[0]=global_forwarding
-uci set passwall2.@global_forwarding[0].tcp_no_redir_ports='disable'
-uci set passwall2.@global_forwarding[0].udp_no_redir_ports='disable'
-uci set passwall2.@global_forwarding[0].tcp_redir_ports='1:65535'
-uci set passwall2.@global_forwarding[0].udp_redir_ports='1:65535'
-uci set passwall2.@global[0].remote_dns='8.8.4.4'
-
-uci set passwall2.Direct=shunt_rules
-uci set passwall2.Direct.network='tcp,udp'
-uci set passwall2.Direct.remarks='IRAN'
-uci set passwall2.Direct.ip_list='0.0.0.0/8
-10.0.0.0/8
-100.64.0.0/10
-127.0.0.0/8
-169.254.0.0/16
-172.16.0.0/12
-192.0.0.0/24
-192.0.2.0/24
-192.88.99.0/24
-192.168.0.0/16
-198.19.0.0/16
-198.51.100.0/24
-203.0.113.0/24
-224.0.0.0/4
-240.0.0.0/4
-255.255.255.255/32
-::/128
-::1/128
-::ffff:0:0:0/96
-64:ff9b::/96
-100::/64
-2001::/32
-2001:20::/28
-2001:db8::/32
-2002::/16
-fc00::/7
-fe80::/10
-ff00::/8
-geoip:ir'
-uci set passwall2.Direct.domain_list='regexp:^.+\.ir$
-geosite:category-ir'
-
-uci set passwall2.myshunt.Direct='_direct'
-
-uci commit passwall2
-
-uci commit system
-
-uci set system.@system[0].hostname=By-AmirHossein
-
-uci commit system
-
-uci set dhcp.@dnsmasq[0].rebind_domain='www.ebanksepah.ir 
-my.irancell.ir'
-
-uci commit
-
 echo -e "${YELLOW}** Installation Completed ** ${ENDCOLOR}"
-echo -e "${MAGENTA} Made With Love By : AmirHossein ${ENDCOLOR}"
 
 rm passwall2x.sh
 
